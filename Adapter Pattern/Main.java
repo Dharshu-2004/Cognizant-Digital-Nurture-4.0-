a7e5f7d3d8361f95f944130d1cd6aa1b19f06849
Dharshini Ravikumar <dharshinimm2004@gmail.com>
2025-06-19 11:44:16 +0530

    Delete Adapter Pattern directory

--- Adapter Pattern/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       PaymentProcessor payPalProcessor = new PayPalAdapter(new PayPalGateway());
-        payPalProcessor.processPayment(500.0);
-}
-}
